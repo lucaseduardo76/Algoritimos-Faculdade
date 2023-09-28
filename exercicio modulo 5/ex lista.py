3e7fb065e3f9490c93fa8e244b50881ccf5abc44
Lucas Eduardo <lesds2001@gmail.com>
2023-09-28 16:49:34 -0300

    adicionando mais exercícios

--- exercicio modulo 5/ex lista.py
+++ exercicio modulo 5/ex lista.py
@@ -4,13 +4,9 @@ def ex1():
     qnt_termos = int(input("Quantos termos essa P.A possui? "));
     razao = int(input("Qual a razão da P.A: "));
     pa = []; 
-
     for i in range(qnt_termos):
         pa.append(termo_atual)
-        termo_atual += razao
-
-        
-    
+        termo_atual += razao       
     print(f"Os termos da da P.A são {pa} \n"
         "A soma dos termos da P.A é {sum(pa)}")
     
@@ -63,11 +59,52 @@ def ex2():
             f"Tempo médio dos nadadores: {t_m/7}")
 
 def ex3():
+    import random
     nomes = []
     cont = 1
     while True:
 
-        nome = input(f"Digite o nome do {cont}° comprador da rifa: ")
-        next = input("Deseja adicionar outra pessoa? ")
-        ver = 's'
+        nome = input(f"Digite o nome do {cont}° comprador da rifa: [Para finalizar digite 0] ")
+
+        if(nome == '0'):
+            break
+        else: 
+            nomes.append(nome)
+            cont += 1
+    
+    print(f"O ganhador do premio foi {random.choice(nomes)}")
+
+
+def ex4():
+    ver = True
+    list_number = []
+    middle_number = 0
+    while ver == True:
+        while True:
+            number = int(input("Digite um numero inteiro positivo: [Para finalizar digite 0] \n"))
+
+            if(number > 0):
+                break
+            elif (number == 0):
+                if(len(list_number) % 2 != 0):
+                    ver = False
+                    break
+                else:
+                    print("Quantidade de itens invalida, adicione mais um numero para encerrar! \n")
+            else:
+                print("Valor inválido! \n")
+
+        list_number.append(number)   
+    middle_number = list_number[int((len(list_number)/2) - 0.5)]
+
+   
+    tot_sum = 1
+    for i in range(middle_number, 0, -1):
+        tot_sum *= i
+
+    
+    print(f"O fatorial de {middle_number} é {tot_sum}!")
+
+ex4()
+    
             
