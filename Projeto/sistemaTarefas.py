7ce8eb27c52cf57184ffcdd77b4dfc6578f38783
Lucas Eduardo <lesds2001@gmail.com>
2023-10-27 00:04:03 -0300

    Adicionando interação entre Json de usuario e de tarefas

--- Projeto/sistemaTarefas.py
+++ Projeto/sistemaTarefas.py
@@ -49,7 +49,7 @@ def limpar_terminal():
     os.system('cls')
 
 
-def validar_data(data_string):
+def validarData(data_string):
     try:
         data = datetime.datetime.strptime(data_string, '%d/%m/%Y').date()
         return data
@@ -57,7 +57,7 @@ def validar_data(data_string):
         return None
     
     
-def formatar_data(data):
+def formatarData(data):
     return data.strftime('%d/%m/%Y')
 
 
@@ -88,7 +88,7 @@ def listarUsuarios():
 while True:
     dadosTarefa = readTarefas()
     dadosUsuario = readUsuarios()
-            
+
     acaoGeral = int(input(f"Bem vindo ao sistema de gerenciamento de tarefas: \n"
         f"Escolha uma das opções abaixo: \n"
         f"1° Gerenciar Usuarios \n"
@@ -98,7 +98,7 @@ while True:
     if acaoGeral == 1:
         acaoUsuario = 0
         while acaoUsuario != 4:
-
+            limpar_terminal()
             idUsuario = 1
             for i in range(len(dadosUsuario)):
                 idUsuario = int(dadosUsuario[i]['id']) + 1
@@ -142,137 +142,165 @@ while True:
                     usuario = int(input("Digite o Id do usuario que deseja excluir: "))
                     
                     
-                    c = 'n!#5'
-                    for u in range(len(dadosUsuario) - 1):
-                        if(dadosUsuario[u]['id'] == usuario):
+                    idInvalido = False
+                    for u in range(len(dadosUsuario)):
+                        indice = u - 1
+                        if(dadosUsuario[indice]['id'] == usuario):
+                            limpar_terminal()
+                            print(f"----------------------------------------- \n"
+                                  f"Id: {dadosUsuario[indice]['id']} \n"
+                                  f"Nome: {dadosUsuario[indice]['nome']} \n"
+                                  f"-----------------------------------------")
                             c = input("Tem certeza que deseja excluir o usuario? [S/N]: ")
+                        
                             if(c == 's' or c == 'S'):
-                                dadosUsuario.pop(u)
+                                dadosUsuario.pop(indice)
                                 updateUsuarios(dadosUsuario)
+                            else:
+                                idInvalido = True
+
+                    limpar_terminal()
+                    if idInvalido:
+                        print('Id não existe!')
+            elif acaoUsuario == 4:
+                limpar_terminal()
+    
+    elif acaoGeral == 2:
+        if len(dadosUsuario) > 0:
+            limpar_terminal()
+
+            idTarefa = 1
+            for i in range(len(dadosTarefa)):        
+                idTarefa = int(dadosTarefa[i]['id']) + 1
+
+            acaoTarefa = int(input(f"Escolha uma das opções abaixo: \n"
+                f"1° Adicionar Tarefas \n"
+                f"2° Alterar Status \n"
+                f"3° Editar Tarefa \n"
+                f"4° Excluir Tarefa \n"
+                f"5° Listar Tarefas \n"
+                f"6° Encerrar Sistema \n"))
+            
+            if acaoTarefa == 1:
+                limpar_terminal()
+                veirfyId = True
+                while veirfyId:                
+                    listarUsuarios()
+                    idResp = int(input("Digite o Id do responsável pela tarefa: "))
+                    for i in range(len(dadosUsuario)):
+                        if dadosUsuario[i]['id'] == idResp:
+                            veirfyId = False
+                        if veirfyId:
+                            print("Id inexistente!")
+                
+                
+                #PARAMOS AQUI  NA VERIFICAÇÃO SE O FUNCIONARIO JÁ TEM ALGUMA TAREFA NESSA DATA
+                verifyData = True
+                while verifyData:
+                    data = input("Digite a data limite para execução da tarefa: (DD/MM/YYYY): ")
+                    if validarData(data):
+                        data = formatarData(validarData(data))
+                        verifyData = False                 
+
+                descricao = input("Digite a descricao da tarefa: ")
+
+                dicionario = {
+                    "id": idTarefa,
+                    "idColaborador": idResp,
+                    "data": data,
+                    "descricao": descricao,
+                }
+                dadosTarefa.append(dicionario)
+                updateTarefas(dadosTarefa)
+                limpar_terminal()
+
+            elif acaoTarefa == 2:
+                limpar_terminal()
+                listarTarefas()
+                if(len(dadosTarefa)):
+                    tarefa = int(input("Digite o numero da tarefa que deseja alterar o status: "))
+                
+                    for i in range(len(dadosTarefa)):
+
+                        if(dadosTarefa[i]['id'] == tarefa):
+                            stat = int(input(f"1  - Em andamento \n"
+                                        f"2 - Finalizada \n"
+                                        f"Selecione o status que deseja aplicar \n"))
+                            upData = ''
+                            if(stat == 1):
+                                upData = 'Em andamento'
+                            elif(stat == 2):
+                                upData = 'Finalizada'
+                            
+                            dadosTarefa[i].update({'status': upData})
+                            updateTarefas(dadosTarefa)
+                    limpar_terminal()     
+
+            elif acaoTarefa == 3:
+                limpar_terminal()
+                listarTarefas()
+                if(len(dadosTarefa)):
+                    tarefa = int(input("Digite o numero da tarefa que deseja editar: "))
+                    
+                    for i in range(len(dadosTarefa)):
+
+                        if(dadosTarefa[i]['id'] == tarefa):
+                            edit = int(input(f"1  - Responsável \n"
+                                        f"2 - Data Limite \n"
+                                        f"3 - Descrição \n"
+                                        f"Selecione o dado que deseja editar: "))
+                            
+                            if(edit == 1):
+                                resp = input("Digite o nome do novo responsável: ")
+                                dadosTarefa[i].update({'responsavel': resp})
+                                updateTarefas(dadosTarefa)
+                                limpar_terminal()
+                            elif(edit == 2):
+                                dat = input("Digite a nova data limite para a tarefa: ")
+                                dadosTarefa[i].update({'data': dat})
+                                updateTarefas(dadosTarefa)
+                                limpar_terminal()
+                            elif(edit == 3):
+                                desc = input("Digite a nova descrição para a tarefa: ")
+                                dadosTarefa[i].update({'descricao': desc})
+                                updateTarefas(dadosTarefa)
+                                limpar_terminal()
+                            else:
+                                limpar_terminal()
+                                print('OPÇÃO INVÁLIDA')
+                    
+            elif acaoTarefa == 4:
+                limpar_terminal()
+                listarTarefas()
+                if(len(dadosTarefa)):
+                    verify = True
+                    while verify:
+                        tarefa = int(input("Digite o numero da tarefa que deseja excluir: "))
+                        if tarefa <= (len(dadosTarefa)) and tarefa > 0:
+                            verify = False
+
+                    c = input("Tem certeza que deseja excluir a tarefa? [S/N]: ")
+
+                    if(c == 's' or c == 'S'):
+                        
+                        dadosTarefa.pop(tarefa - 1)
+
+                        for i in range(len(dadosTarefa)):
+                            dadosTarefa[i].update({'id': i+1})
+                        updateTarefas(dadosTarefa)
 
 
                     limpar_terminal()
-                    if c == 'n!#5':
-                        print('Id não existe!')
-    
-    elif acaoGeral == 2:
-        idTarefa = 1
-        for i in range(len(dadosTarefa)):        
-            idTarefa = int(dadosTarefa[i]['id']) + 1
-
-        acaoTarefa = int(input(f"Escolha uma das opções abaixo: \n"
-            f"1° Adicionar Tarefas \n"
-            f"2° Alterar Status \n"
-            f"3° Editar Tarefa \n"
-            f"4° Excluir Tarefa \n"
-            f"5° Listar Tarefas \n"
-            f"6° Encerrar Sistema \n"))
-        
-        if acaoTarefa == 1:
-            limpar_terminal()
-            listarUsuarios()
-            responsavel = input("Digite o Id do responsável pela tarefa: ")
-            data = input("Digite a data limite para execução da tarefa: ")
-            descricao = input("Digite a descricao da tarefa: ")
-            
-
-
-            dicionario = {
-                "id": idTarefa,
-                "responsavel": responsavel,
-                "data": data,
-                "descricao": descricao,
-                "status": "Não iniciada"
-            }
-            dadosTarefa.append(dicionario)
-            updateTarefas(dadosTarefa)
-            limpar_terminal()
-        elif acaoTarefa == 2:
-            limpar_terminal()
-            listarTarefas()
-            if(len(dadosTarefa)):
-                tarefa = int(input("Digite o numero da tarefa que deseja alterar o status: "))
-            
-                for i in range(len(dadosTarefa)):
-
-                    if(dadosTarefa[i]['id'] == tarefa):
-                        stat = int(input(f"1  - Em andamento \n"
-                                    f"2 - Finalizada \n"
-                                    f"Selecione o status que deseja aplicar \n"))
-                        upData = ''
-                        if(stat == 1):
-                            upData = 'Em andamento'
-                        elif(stat == 2):
-                            upData = 'Finalizada'
-                        
-                        dadosTarefa[i].update({'status': upData})
-                        updateTarefas(dadosTarefa)
-                limpar_terminal()     
-
-        elif acaoTarefa == 3:
-            limpar_terminal()
-            listarTarefas()
-            if(len(dadosTarefa)):
-                tarefa = int(input("Digite o numero da tarefa que deseja editar: "))
                 
-                for i in range(len(dadosTarefa)):
-
-                    if(dadosTarefa[i]['id'] == tarefa):
-                        edit = int(input(f"1  - Responsável \n"
-                                    f"2 - Data Limite \n"
-                                    f"3 - Descrição \n"
-                                    f"Selecione o dado que deseja editar: "))
-                        
-                        if(edit == 1):
-                            resp = input("Digite o nome do novo responsável: ")
-                            dadosTarefa[i].update({'responsavel': resp})
-                            updateTarefas(dadosTarefa)
-                            limpar_terminal()
-                        elif(edit == 2):
-                            dat = input("Digite a nova data limite para a tarefa: ")
-                            dadosTarefa[i].update({'data': dat})
-                            updateTarefas(dadosTarefa)
-                            limpar_terminal()
-                        elif(edit == 3):
-                            desc = input("Digite a nova descrição para a tarefa: ")
-                            dadosTarefa[i].update({'descricao': desc})
-                            updateTarefas(dadosTarefa)
-                            limpar_terminal()
-                        else:
-                            limpar_terminal()
-                            print('OPÇÃO INVÁLIDA')
-                
-        elif acaoTarefa == 4:
-            limpar_terminal()
-            listarTarefas()
-            if(len(dadosTarefa)):
-                verify = True
-                while verify:
-                    tarefa = int(input("Digite o numero da tarefa que deseja excluir: "))
-                    if tarefa <= (len(dadosTarefa)) and tarefa > 0:
-                        verify = False
-
-                c = input("Tem certeza que deseja excluir a tarefa? [S/N]: ")
-
-                if(c == 's' or c == 'S'):
                     
-                    dadosTarefa.pop(tarefa - 1)
-
-                    for i in range(len(dadosTarefa)):
-                        dadosTarefa[i].update({'id': i+1})
-                    updateTarefas(dadosTarefa)
-
-
+            elif acaoTarefa == 5:        
                 limpar_terminal()
+                listarTarefas()
+            elif acaoTarefa == 6:
+                print('Obrigado!')
+                break
+        else:
+           print('É necessário cadastrar um usuario para criar uma tarefa!')  
             
-                
-        elif acaoTarefa == 5:        
-            limpar_terminal()
-            listarTarefas()
-        elif acaoTarefa == 6:
-            print('Obrigado!')
-            break
-        
-        
 
 
--- teste.py
+++ teste.py
@@ -5,7 +5,7 @@ def validar_data(data_string):
         data = datetime.datetime.strptime(data_string, '%d/%m/%Y').date()
         return data
     except ValueError:
-        return None
+        return False
     
 def formatar_data(data):
     return data.strftime('%d/%m/%Y')
@@ -15,7 +15,7 @@ data_digitada = input("Digite uma data no formato YYYY-MM-DD: ")
 
 data_valida = validar_data(data_digitada)
 
-if data_valida:
+if validar_data(data_digitada):
     print(f"A data {formatar_data(data_valida)} é válida.")
 else:
     print("A data digitada não é válida. Certifique-se de usar o formato YYYY-MM-DD.")
